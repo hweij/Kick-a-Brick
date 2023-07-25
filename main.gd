e81a1a9cedbf94aad324dc59bc4c317ab7bc2b1f
hweij <henk000@gmail.com>
2023-07-25 08:13:44 +0200

    cleanup: remove unused references to controllers

--- main.gd
+++ main.gd
@@ -2,9 +2,6 @@ extends Node3D
 
 var xr_interface: XRInterface
 
-var leftController: XRController3D
-var rightController: XRController3D
-
 func _ready():
 	xr_interface = XRServer.find_interface("OpenXR")
 	if xr_interface and xr_interface.is_initialized():
@@ -17,16 +14,11 @@ func _ready():
 		get_viewport().use_xr = true
 	else:
 		print("OpenXR not initialized, please check if your headset is connected")
-
-	# Store references to the controllers, might remove this later
-	leftController = get_node("Player/XROrigin3D/LeftController");
-	rightController = get_node("Player/XROrigin3D/LeftController");
 	
 	create_wall()
 	
 func _process(_delta):
 	pass
-	# print(leftController.get_input("trigger_click"))
 
 func create_wall():
 	const colors = [
