a1a70f10f58ff0dcca3dc0fd66403d8679b2cd95
hweij <henk000@gmail.com>
2023-07-25 10:37:09 +0200

    cleanup: code cleanup, brick rename

--- RightController.gd
+++ RightController.gd
@@ -1,40 +1,45 @@
 extends XRController3D
 
-
-var trigger_click = false
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-# func _process(delta):
-#	pass
-
 # Range of the ray
 const RAY_LENGTH = 20
-# Force for kicking rigid objects
-const FORCE = 10
 
+# Force for kicking bricks
+const KICK_FORCE = 10
+
+# Current trigger state, for trigger event detection
+var trigger_click = false
+
+# Called when the node enters the scene tree for the first time, not used for now.
+func _ready():
+	pass
+
+# Called every display frame, not used for now.
+func _process(_delta):
+	pass
+
+# We do things here since we are dealing with physics.
 func _physics_process(_delta):
+	# Ray tracing
 	var space_state = get_world_3d().direct_space_state
-
-	var origin = self.global_position
+	# Start of ray: the controller's position
+	var origin := self.global_position
+	# Direction of the ray, pointing forward
 	var dir := -self.global_transform.basis.z
-	var end = origin + (dir * RAY_LENGTH)
-	var query = PhysicsRayQueryParameters3D.create(origin, end)
-	query.collide_with_areas = true
-
-	var result = space_state.intersect_ray(query)
+	# End of the ray
+	var end := origin + (dir * RAY_LENGTH)
+	var query := PhysicsRayQueryParameters3D.create(origin, end)
+	var result := space_state.intersect_ray(query)
 	
-	var push = false
-	var tc = get_input("trigger_click")
+	# Trigger detection
+	var kick = false
+	var tc := self.get_float("trigger_click") as bool
 	if tc != trigger_click:
 		trigger_click = tc
-		push = trigger_click
+		kick = trigger_click
 
-	if push && result:
+	# Kicking bricks
+	if kick && result:
 		var obj = result.collider
 		if obj is RigidBody3D:
 			var hit_pos = result.position - obj.position
-			result.collider.apply_impulse(dir * FORCE, hit_pos)
+			result.collider.apply_impulse(dir * KICK_FORCE, hit_pos)
--- main.gd
+++ main.gd
@@ -5,7 +5,7 @@ var xr_interface: XRInterface
 func _ready():
 	xr_interface = XRServer.find_interface("OpenXR")
 	if xr_interface and xr_interface.is_initialized():
-		print("OpenXR initialised successfully")
+		print("OpenXR initialised!")
 
 		# Turn off v-sync!
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
@@ -13,7 +13,7 @@ func _ready():
 		# Change our main viewport to output to the HMD
 		get_viewport().use_xr = true
 	else:
-		print("OpenXR not initialized, please check if your headset is connected")
+		print("OpenXR not initialized, check your setup!")
 	
 	create_wall()
 	
@@ -22,9 +22,9 @@ func _process(_delta):
 
 func create_wall():
 	# Wall dimensions
-	const num_rows = 9
+	const num_rows = 9	# Best to use an odd number
 	const num_cols = 20
-	# Brick size (should match size of bricks defined in rigid_brick scene
+	# Brick size (should match size of bricks defined in rigid_brick scene)
 	const w = 0.3
 	const h = 0.1
 	# Brick colors
@@ -35,24 +35,22 @@ func create_wall():
 		Color(0.2, 0.0, 0.8)
 	]
 	# Brick materials from colors
-	var materials = colors.map(
+	var materials := colors.map(
 		func (c):
-			var mat = StandardMaterial3D.new()
+			var mat := StandardMaterial3D.new()
 			mat.albedo_color = c
 			return mat
 	)
 	var num_materials := materials.size()
-	var scene = load("res://rigid_brick.tscn")
-	var boxes = []
+	var scene = load("res://brick.tscn")
 	const offset_y = h / 2
 	for row in num_rows:
 		# Even rows have 1 more brick than uneven rows.
 		# To create a stable wall, make sure the number of rows is uneven
-		var n = num_cols if (row % 2) else (num_cols - 1)
+		var n := num_cols if (row % 2) else (num_cols - 1)
 		var offset_x := (n as float) * w / 2.0
 		for col in n:
 			var box = scene.instantiate()
 			box.get_node("Mesh").set_surface_override_material(0, materials[randi_range(0, num_materials - 1)])
 			box.transform.origin = Vector3(col * w - offset_x, row * h + offset_y, -2)
 			self.add_child(box)
-			boxes.push_back(box)
