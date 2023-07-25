d91004f0c72b8bf844ceb6e95d0cfa327528caf1
hweij <henk000@gmail.com>
2023-07-25 08:54:53 +0200

    refactor: clean up main scene code

--- main.gd
+++ main.gd
@@ -21,33 +21,38 @@ func _process(_delta):
 	pass
 
 func create_wall():
+	# Wall dimensions
+	const num_rows = 9
+	const num_cols = 20
+	# Brick size (should match size of bricks defined in rigid_brick scene
+	const w = 0.3
+	const h = 0.1
+	# Brick colors
 	const colors = [
 		Color(0.5, 0.0, 0.5),
 		Color(0.5, 0.5, 0.0),
 		Color(0.0, 0.5, 0.5),
 		Color(0.2, 0.0, 0.8)
 	]
-	var materials = [];
-	for i in 4:
-		var mat = StandardMaterial3D.new()
-		mat.albedo_color = colors[i]
-		materials.push_back(mat)
-	const num_rows = 6
-	const num_cols = 20
-	const w = 0.3
-	const h = 0.1
-	const offset_x = num_cols * w / 2
-	const offset_y = h / 2
+	# Brick materials from colors
+	var materials = colors.map(
+		func (c):
+			var mat = StandardMaterial3D.new()
+			mat.albedo_color = c
+			return mat
+	)
+	var num_materials := materials.size()
 	var scene = load("res://rigid_brick.tscn")
 	var boxes = []
+	const offset_y = h / 2
 	for row in num_rows:
-		for col in num_cols:
+		# Even rows have 1 more brick than uneven rows.
+		# To create a stable wall, make sure the number of rows is uneven
+		var n = num_cols if (row % 2) else (num_cols - 1)
+		var offset_x := (n as float) * w / 2.0
+		for col in n:
 			var box = scene.instantiate()
-			#var mat = StandardMaterial3D.new()
-			#var mat = box.get_node("Mesh").get_surface_override_material(0)
-			#mat.albedo_color = Color(randf(), randf(), randf())
-			box.get_node("Mesh").set_surface_override_material(0, materials[randi() % 4])
+			box.get_node("Mesh").set_surface_override_material(0, materials[randi_range(0, num_materials - 1)])
 			box.transform.origin = Vector3(col * w - offset_x, row * h + offset_y, -2)
 			self.add_child(box)
 			boxes.push_back(box)
-
